4dc921a58e17a023e911b07106fa101ab7e3f0f0
Ravi Rawat <ravi@arrowai.com>
2021-05-22 12:36:07 +0530

    changes

--- CrudApp/assets/js/index.js
+++ CrudApp/assets/js/index.js
@@ -12,7 +12,7 @@ $('#update_user').submit(function (event){
     })
     console.log(data.id)
     var request={
-        "url": `http://localhost:3000/api/users/${data.id}`,
+        "url": `/api/users/${data.id}`,
         "method":"put",
         "data":data
     }
@@ -29,7 +29,7 @@ if(window.location.pathname=="/")
         var id = $(this).attr("data-id");
         console.log(id)
         var request={
-            "url": `http://localhost:3000/api/users/${id}`,
+            "url": `/api/users/${id}`,
             "method":"delete",
         }
         if(confirm("Do you reallly want to delete this data")){
--- CrudApp/server/route/routes.js
+++ CrudApp/server/route/routes.js
@@ -1,40 +1,49 @@
-const express=require('express');
+const express = require('express');
 const route = express.Router();
 const controller = require('../controller/controller');
 const axios = require('axios');
 // const { response } = require('express');
 
 
-route.get('/',(req,res)=>{ 
-    axios.get('http://localhost:8080/api/users')
-        .then(function(response){
-            // console.log(response.data)
-            res.render('index',{users:response.data})
-        })
-        .catch(err=>{
-            console.log(err)
-        })
+route.get('/', async (req, res) => {
+    controller.find(req).then(response => {
+        res.render('index', { users: response })
+    })
+    // axios.get('http://localhost:8080/api/users')
+    //     .then(function(response){
+    //         // console.log(response.data)
+    //         res.render('index',{users:response.data})
+    //     })
+    //     .catch(err=>{
+    //         console.log(err)
+    //     })
 })
-route.get('/add-user',(req,res)=>{
+route.get('/add-user', (req, res) => {
     res.render('add_user')
 })
 
-route.get('/update-user',(req,res)=>{
-    axios.get('http://localhost:8080/api/users',{params:{id:req.query.id}})
-        .then(function(userdata){
-            console.log(userdata.data.name)
-            res.render("update_user",{user:userdata})
-        })
-        .catch(err=>{
-            res.send(err)
-        })
+route.get('/update-user', (req, res) => {
+    // req.params = { id: req.query.id };
+    console.log(req.query);
+    controller.find(req).then(response => {
+        console.log(response);
+        res.render("update_user", { user: response })
+})
+    // axios.get('http://localhost:8080/api/users',{params:{id:req.query.id}})
+    //     .then(function(userdata){
+    //         console.log(userdata.data.name)
+    //         res.render("update_user",{user:userdata})
+    //     })
+    //     .catch(err=>{
+    //         res.send(err)
+    //     })
 })
 
 // API
 
-route.post('/api/users',controller.create);
-route.get('/api/users',controller.find);
-route.put('/api/users/:id',controller.update);
-route.delete('/api/users/:id',controller.delete);
+route.post('/api/users', controller.create);
+route.get('/api/users', controller.find);
+route.put('/api/users/:id', controller.update);
+route.delete('/api/users/:id', controller.delete);
 
 module.exports = route;
